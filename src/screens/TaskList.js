dfdde725668dd2ccf091a9b7943c23bee3b1663b
erikepc <erike45@hotmail.com>
2025-03-11 22:18:32 -0300

    cria o componente Task

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -1,28 +1,26 @@
-import { Text, View, StyleSheet, ImageBackground, TouchableOpacity} from "react-native";
+import { Text, View, StyleSheet, ImageBackground, TouchableOpacity } from "react-native";
 
-import Icon from "react-native-vector-icons/FontAwesome"
+import Icon from "react-native-vector-icons/FontAwesome";
 
 import moment from "moment-timezone";
-import 'moment/locale/pt-br'
+import 'moment/locale/pt-br';
 
 import todayImage from '../../assets/imgs/today.jpg'
+import Task from "../../components/task";
 
-export default function TaskList(){
+export default function TaskList() {
+    
+    const userTimeZone = moment.tz.guess(); //detecta fuso horario do dispositivo
+    const today = moment().tz("America/Sao_Paulo").locale('pt-br').format('ddd, D [de] MMMM')
+    //const today = moment().locale('pt-br').format('ddd, D [de] MMMM')
 
-    // moment.tz.setDefault('America/Sao_Paulo'); - Define fuso horario de um local específico
-
-    const userTimeZone = moment.tz.guess(); // Detecta fuso horario do dispositivo 
-    console.log(userTimeZone)
-    const today = moment().tz('America/Sao_Paulo').locale('pt-br').format('ddd, D [de] MMMM')
-
-    // const today = moment().locale('pt-br').format('ddd, D [de] MMMM')
     return(
-        <View style={styles.container}>
-            
+        <View style={styles.container}> 
+
             <ImageBackground source={todayImage} style={styles.background}>
                 <View style={styles.iconBar}>
                     <TouchableOpacity onPress={() => console.warn('oi')}>
-                        <Icon name="eye" size={20} color={'#fff'}/>
+                        <Icon name="eye" size={60} color={'#fff'}/>
                     </TouchableOpacity>
                 </View>
 
@@ -33,11 +31,10 @@ export default function TaskList(){
             </ImageBackground>
 
             <View style={styles.taskList}>
-
+                <Task/>
             </View>
-            <TouchableOpacity style={styles.addButton} activeOpacity={0.7} onPress={() => console.warn('+')}>
-                <Icon name="plus" size={20} color={'#fff'}/>
-
+            <TouchableOpacity style={styles.addButton}activeOpacity={0.7}onPress={() => console.warn('+')}>
+                <Icon name="plus" size={40} color={'#fff'}/>
             </TouchableOpacity>
 
         </View>
@@ -47,44 +44,43 @@ export default function TaskList(){
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+       
     },
     background: {
         flex: 3,
+
     },
     taskList: {
-        flex: 7
+        flex: 7,
     },
     titleBar: {
         flex: 1,
-        justifyContent: 'flex-end'
-
+        justifyContent: 'flex-end',
     },
     title: {
         color: 'white',
         fontSize: 50,
         marginLeft: 20,
         marginBottom: 20
-
     },
     subtitle: {
         color: 'white',
         fontSize: 20,
         marginLeft: 20,
         marginBottom: 30
-    }, 
+    },
     iconBar: {
         flexDirection: 'row',
         marginHorizontal: 20,
         justifyContent: 'flex-end',
-        marginTop: 25
+        marginTop: 20
     },
     addButton: {
         position: 'absolute',
         right: 30,
         bottom: 30,
-        width: 50,
-        height: 50,
-        borderRadius:25,
+        width: 40,
+        borderRadius: 25, 
         backgroundColor: '#B13B44',
         justifyContent: 'center',
         alignItems: 'center'
