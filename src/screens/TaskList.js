9e9cc1b78b3cfc4060b61bc24ba766bbfa296478
erikepc <erike45@hotmail.com>
2025-03-20 22:14:49 -0300

    cria a funcionalidade de tornar a task pendente/concluída

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Text, View, StyleSheet, ImageBackground, TouchableOpacity, FlatList } from "react-native";
 
 import Icon from "react-native-vector-icons/FontAwesome";
@@ -8,106 +9,54 @@ import 'moment/locale/pt-br';
 import todayImage from '../../assets/imgs/today.jpg'
 import Task from "../../components/task";
 
+const taskDB = [
+    {
+        id: Math.random(),
+        desc: 'Elaborar o MER do TCC',
+        estimateAt: new Date(),
+        doneAt: null
+    },
+    {
+        id: Math.random(),
+        desc: 'Ajustar o FIGMA',
+        estimateAt: new Date(),
+        doneAt: new Date()
+    },
+    {
+        id: Math.random(),
+        desc: 'Desenvolver o Backend do sistema',
+        estimateAt: new Date(),
+        doneAt: null
+    }
+]
+
 export default function TaskList() {
 
-    const tasks = [
-        {
-            id: Math.random(),
-            desc: 'Elaborar o MER do TCC',
-            estimateAt: new Date(),
-            doneAt: null                  
-        },
-        {
-            id: Math.random(),
-            desc: 'Ajustar o FIGMA',
-            estimateAt: new Date(),
-            doneAt: new Date()                   
-        },
-        {
-            id: Math.random(),
-            desc: 'Desenvolver o Backend do sistema',
-            estimateAt: new Date(),
-            doneAt: null                   
-        },
-        {
-            id: Math.random(),
-            desc: 'Desenvolver o Backend do sistema',
-            estimateAt: new Date(),
-            doneAt: new Date()                   
-        },
-        {
-            id: Math.random(),
-            desc: 'Desenvolver o Backend do sistema',
-            estimateAt: new Date(),
-            doneAt: null                   
-        },
-        {
-            id: Math.random(),
-            desc: 'Desenvolver o Backend do sistema',
-            estimateAt: new Date(),
-            doneAt: new Date()                   
-        },
-        {
-            id: Math.random(),
-            desc: 'Desenvolver o Backend do sistema',
-            estimateAt: new Date(),
-            doneAt: new Date()                   
-        },
-        {
-            id: Math.random(),
-            desc: 'Desenvolver o Backend do sistema',
-            estimateAt: new Date(),
-            doneAt: new Date()                   
-        },
-        {
-            id: Math.random(),
-            desc: 'Desenvolver o Backend do sistema',
-            estimateAt: new Date(),
-            doneAt: new Date()                   
-        },
-        {
-            id: Math.random(),
-            desc: 'Desenvolver o Backend do sistema',
-            estimateAt: new Date(),
-            doneAt: new Date()                   
-        },
-        {
-            id: Math.random(),
-            desc: 'Desenvolver o Backend do sistema',
-            estimateAt: new Date(),
-            doneAt: new Date()                   
-        },
-        {
-            id: Math.random(),
-            desc: 'Desenvolver o Backend do sistema',
-            estimateAt: new Date(),
-            doneAt: new Date()                   
-        },
-    ]
-    
+    const [tasks, setTasks] = useState([...taskDB])
+
     const userTimeZone = moment.tz.guess(); //detecta fuso horario do dispositivo
     const today = moment().tz("America/Sao_Paulo").locale('pt-br').format('ddd, D [de] MMMM')
     //const today = moment().locale('pt-br').format('ddd, D [de] MMMM')
 
-    toggleTask = taskId => {
+    const toggleTask = taskId => {
         const taskList = [...tasks]
-        TaskList.forEach(task => {
-            if(task.id === taskId) {
+        taskList.forEach(task => {
+            if (task.id === taskId) {
                 task.doneAt = task.doneAt ? null : new Date()
             }
         });
 
-        tasks = taskList
+        setTasks(taskList)
     }
 
-    
-    return(
-        <View style={styles.container}> 
+
+    return (
+        <View style={styles.container}>
 
             <ImageBackground source={todayImage} style={styles.background}>
                 <View style={styles.iconBar}>
                     <TouchableOpacity onPress={() => console.warn('oi')}>
-                        <Icon name="eye" size={60} color={'#fff'}/>
+                        <Icon name="eye" size={20} color={'#fff'} />
                     </TouchableOpacity>
                 </View>
 
@@ -121,12 +70,12 @@ export default function TaskList() {
                 <FlatList
                     data={tasks}
                     keyExtractor={item => `${item.id}`}
-                    renderItem={({item}) => <Task {...item} />}
+                    renderItem={({ item }) => <Task {...item} onToggleTask={toggleTask}/>}
                 />
             </View>
 
-            <TouchableOpacity style={styles.addButton}activeOpacity={0.7}onPress={() => console.warn('+')}>
-                <Icon name="plus" size={40} color={'#fff'}/>
+            <TouchableOpacity style={styles.addButton} activeOpacity={0.7} onPress={() => console.warn('+')}>
+                <Icon name="plus" size={40} color={'#fff'} />
             </TouchableOpacity>
 
         </View>
@@ -136,7 +85,7 @@ export default function TaskList() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-       
+
     },
     background: {
         flex: 3,
@@ -172,7 +121,7 @@ const styles = StyleSheet.create({
         right: 30,
         bottom: 30,
         width: 40,
-        borderRadius: 25, 
+        borderRadius: 25,
         backgroundColor: '#B13B44',
         justifyContent: 'center',
         alignItems: 'center'
