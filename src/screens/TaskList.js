a46a02e3f56d5336849875e1d902d27f969ed958
erikepc <erike45@hotmail.com>
2025-05-06 22:34:38 -0300

    implementa a funcionalidade de salvar

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { Text, View, StyleSheet, ImageBackground, TouchableOpacity, FlatList } from "react-native"
+import { Text, View, StyleSheet, ImageBackground, TouchableOpacity, FlatList, Alert } from "react-native"
 
 import Icon from "react-native-vector-icons/FontAwesome"
 
@@ -45,7 +45,7 @@ export default function TaskList() {
 
     useEffect(() => {
         filterTasks()
-    }, [showDoneTasks])
+    }, [showDoneTasks, tasks])
 
     const toggleTask = taskId => {
         const taskList = [...tasks]
@@ -75,6 +75,22 @@ export default function TaskList() {
         }
 
         setVisibleTasks(visibleTasks)
+    }
+
+    const addTask = newTask => {
+        if(!newTask.desc || !newTask.desc.trim()){
+            Alert.alert('Dados Inválidos', 'Descrição não informada!')
+        }
+
+        const tempTasks = [...tasks]
+        tempTasks.push({
+            id: Math.random(),
+            desc: newTask.desc,
+            estimateAt: newTask.date,
+            doneAt: null
+        })
+        setTasks(tempTasks)
+        setShowAddTask(false)
     }
 
     return (
